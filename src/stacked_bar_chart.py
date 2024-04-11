48f8de99c9e8ba837fd3966f2d379f2b02a6b7be
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2024-04-11 06:44:59 -0400

    dominant medals v1

--- src/stacked_bar_chart.py
+++ src/stacked_bar_chart.py
@@ -2,28 +2,74 @@ import plotly.express as px
 
 from template import MEDAL_COLORS
 
+
 def get_plot(df, type):
     title = f"Top 5 Countries by {type.capitalize() + ' Medals'}"
-    
+
     if type == "total medals":
-        fig = px.bar(df,
-                     y="Country",
-                     x=["Gold", "Silver", "Bronze"],
-                     title=title,
-                     labels={"value": "Medals", "variable": "Medal Type"},
-                     color_discrete_map=MEDAL_COLORS,
-                     orientation="h",
-                     category_orders={"Country": df["Country"].tolist()})
-        fig.update_layout(barmode="stack") 
+        fig = px.bar(
+            df,
+            y="Country",
+            x=["Gold", "Silver", "Bronze"],
+            title=title,
+            labels={"value": "Medals", "variable": "Medal Type"},
+            color_discrete_map=MEDAL_COLORS,
+            orientation="h",
+            category_orders={"Country": df["Country"].tolist()},
+        )
+        fig.update_layout(barmode="stack")
     else:
         bar_color = MEDAL_COLORS.get(type.capitalize(), "black")
-        fig = px.bar(df,
-                     y="Country",
-                     x=type.capitalize(),
-                     title=title,
-                     color_discrete_sequence=[bar_color],
-                     orientation="h",
-                     category_orders={"Country": df["Country"].tolist()})
-    fig.update_layout(legend_orientation="h", legend=dict(y=-0.2, xanchor="center", x=0.5))
+        fig = px.bar(
+            df,
+            y="Country",
+            x=type.capitalize(),
+            title=title,
+            color_discrete_sequence=[bar_color],
+            orientation="h",
+            category_orders={"Country": df["Country"].tolist()},
+        )
+    fig.update_layout(
+        legend_orientation="h", legend=dict(y=-0.2, xanchor="center", x=0.5)
+    )
     return fig
 
+
+def get_top_plot(df, type, graph_type):
+    title = f"Top 5 {graph_type.capitalize()} with the most {type.capitalize()} Medals by Discipline"
+    hover_column = "athlete_name" if graph_type == "athlete" else "country"
+
+    fig = px.bar(
+        df,
+        y="discipline",
+        x=type,
+        title=title,
+        orientation="h",
+        category_orders={"Discipline": df["discipline"].unique()},
+        hover_data={hover_column: True},
+        color=type,
+        color_continuous_scale=px.colors.sequential.Pinkyl,
+    )
+    fig.update_traces(
+        hovertemplate=(
+            "<b>%{customdata[0]}</b><br>"
+            f"{type.capitalize()} Medals: %{{x}}<br>"
+            f"This represents %{{customdata[1]:.2f}} % of the {type.capitalize()} medals in %{{y}}<extra></extra>"
+        ),
+        customdata=df[[hover_column, "percent"]],
+        marker_line_color="black",
+        marker_line_width=1.5,
+        opacity=1,
+    )
+    fig.update_xaxes(
+        title_text=f"Number of {type.capitalize()} Medals",
+    )
+    fig.update_yaxes(
+        autorange="reversed",
+        title_text="Discipline",
+    )
+    fig.update_layout(
+        showlegend=False,
+        coloraxis_showscale=False,
+    )
+    return fig
